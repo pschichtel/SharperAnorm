17d25171374fa141fc5d32914de14df88bb0860b
Phillip Schichtel <phillip@schich.tel>
2020-04-28 23:32:44 +0200

    Split out the IRunner interface

--- SharperAnorm/Runner.cs
+++ SharperAnorm/Runner.cs
@@ -8,13 +8,6 @@ using System.Threading.Tasks;
 
 namespace SharperAnorm
 {
-    public interface IRunner<TRow>
-    {
-        Task<IEnumerable<T>> Run<T>(Query q, RowParser<T, TRow> p);
-        Task RunNoResult(Query q);
-        Task<T> RunSingle<T>(Query q, RowParser<T, TRow> p);
-    }
-
     public abstract class Runner<TRow> : IRunner<TRow>
     {
         private readonly Func<Task<DbConnection>> _connectionProvider;
